a247c0b6825450a557da0f6748c72bc09b9a65aa
Merlin Cornehl <mcornehl@mkw.eu>
2023-04-08 16:40:47 +0200

    impl PageTransition component

---
+++ src/lib/components/PageTransition.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import { fade, type FadeParams } from 'svelte/transition';
+
+	/**
+	 * Props
+	 */
+	export let inParams: FadeParams = {
+		delay: 150,
+		duration: 75
+	};
+	export let outParams: FadeParams = {
+		duration: 75
+	};
+</script>
+
+<main
+	class="container mx-auto mt-24"
+	in:fade={inParams}
+	out:fade={outParams}>
+	<slot />
+</main>
