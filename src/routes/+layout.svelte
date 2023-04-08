e8cca1bf01dd381144934f9ec38f39fe2a941a8d
Merlin Cornehl <mcornehl@mkw.eu>
2023-04-08 16:41:39 +0200

    upd root layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,18 +1,38 @@
 <script lang="ts">
 	import '../app.scss'
-	import Navbar from './Navbar.svelte'
-	import type { Route } from '$types/app'
+	import { invalidate } from '$app/navigation'
+	import { routes } from '$lib/globals'
+	import Navbar from '../lib/components/Navigation/Navbar.svelte'
+	import { onMount } from 'svelte'
+	import type { LayoutData } from './$types'
+
+	/**
+	 * Props
+	 */
+	export let data: LayoutData
 
 	/**
 	 * State
 	 */
-	const routes: Route[] = [{ name: 'Home', href: '/' }]
-	const authenticated = false
+	$: ({ supabase, session } = data)
+
+	/**
+	 * Lifecycle methods
+	 */
+	onMount(() => {
+		const {
+			data: { subscription }
+		} = supabase.auth.onAuthStateChange((event, _session) => {
+			if (_session?.expires_at !== session?.expires_at) {
+				invalidate('supabase:auth')
+			}
+		})
+
+		return () => subscription.unsubscribe()
+	})
 </script>
 
 <Navbar
 	{routes}
-	{authenticated} />
-<main class="container">
-	<slot />
-</main>
+	authenticated={!!session} />
+<slot />
